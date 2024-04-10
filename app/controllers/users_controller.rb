d3c7161dfa1128c2c6805f3f1a41952b7ce8ca02
Thinley24 <thinleyjigme705@gmail.com>
2024-04-10 20:34:22 +0600

    removed comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,16 +12,4 @@ class UsersController < ApplicationController
   def user_params
     current_user.slice(:nickname, :name, :picture, :updated_at, :email, :email_verified, :sub)
   end
-
-  # def profile
-  #   @user = User.new(
-  #     nickname: 'Customer',
-  #     name: 'One Customer',
-  #     picture: 'https://cdn.auth0.com/blog/hello-auth0/auth0-user.png',
-  #     updated_at: '2021-05-04T21:33:09.415Z',
-  #     email: 'customer@example.com',
-  #     email_verified: false,
-  #     sub: 'auth0|12345678901234567890'
-  #   )
-  # end
 end
